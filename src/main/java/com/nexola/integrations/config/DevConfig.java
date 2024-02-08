c79e371c14518645d0fe3f09a2066b3f02c4c6e0
nexola <vitormatheusfv@gmail.com>
2024-02-08 18:23:30 -0300

    Padrão strategy para usar mais de um EmailService

---
+++ src/main/java/com/nexola/integrations/config/DevConfig.java
@@ -0,0 +1,17 @@
+package com.nexola.integrations.config;
+
+import com.nexola.integrations.services.EmailService;
+import com.nexola.integrations.services.SendGridEmailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Bean
+    public EmailService emailService() {
+        return new SendGridEmailService();
+    }
+}
---
+++ src/main/java/com/nexola/integrations/services/SendGridEmailService.java
@@ -0,0 +1,44 @@
+package com.nexola.integrations.services;
+
+import com.nexola.integrations.dto.EmailDTO;
+import com.nexola.integrations.services.exceptions.EmailException;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+
+public class SendGridEmailService implements EmailService {
+
+    @Autowired
+    private SendGrid sendGrid;
+
+    public void sendEmail(EmailDTO dto) {
+        Email from = new Email(dto.getFromEmail(), dto.getFromName());
+        Email to = new Email(dto.getTo());
+        Content content = new Content(dto.getContentType(), dto.getBody());
+        Mail mail = new Mail(from, dto.getSubject(), to, content);
+
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sendGrid.api(request);
+            if (response.getStatusCode() >= 400 && response.getStatusCode() <= 500) {
+                throw new EmailException(response.getBody());
+            }
+
+        } catch (IOException e) {
+            throw new EmailException(e.getMessage());
+        }
+    }
+}
